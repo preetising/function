cc622216e660dd431c69643e92a644d9bd096da6
preetising <84845068+preetising@users.noreply.github.com>
2021-06-13 22:25:23 +0530

    Add files via upload

---
+++ fun_2.py
@@ -0,0 +1,7 @@
+def attendance(name,status="absent"):
+    print(name,"is",status," today")
+
+attendance("kartik","present")
+attendance("sonu")
+attendance("vishal","present")
+attendance("umesh") 
